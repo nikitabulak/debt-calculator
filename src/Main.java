1d2b87676391ab577edc68cd96ff1cdeb419fa62
nikitabulak <nikitabulak321@yandex.ru>
2022-03-14 20:46:58 +0300

    feat: first release

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import calculator.DebtCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        DebtCalculator debtCalculator = new DebtCalculator();
+        debtCalculator.calculateDebts();
+     }
+}
